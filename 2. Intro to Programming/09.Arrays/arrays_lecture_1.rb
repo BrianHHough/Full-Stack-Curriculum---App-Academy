9c6e2f26896677b85914a99b785fd04b0adb8243
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2020-01-21 22:31:23 -0500

    Add files via upload

---
+++ 2. Intro to Programming/09.Arrays/arrays_lecture_1.rb
@@ -0,0 +1,33 @@
+#Array data type = to stay organized in code
+# Way to store multiple other data
+
+letters = ["a", "b", "c"]
+
+print letters 
+
+# To find the number of things in an array, use length
+
+letters = ["a", "b", "c", "d"]
+
+puts letters.length
+
+# How to get a single element of an array --> specify its index
+
+letters = ["a", "b", "c", "d"]
+
+puts letters[0]
+puts letters[1]
+
+
+my_arr = ["alvin", true, false, 42]
+
+print my_arr[1]
+
+
+names = [
+    "Jeff",
+    "Tommy",
+    "Jia"
+]
+
+print names
---
+++ 2. Intro to Programming/09.Arrays/arrays_lecture_2.rb
@@ -0,0 +1,18 @@
+# Two ways to manipulate arrays after the fact: SHOVEL or ASSIGN an element into an array
+
+
+#initialize this array with 3 names
+names = ["Maurice", "Mashu", "Kurstie"]
+
+# ASSIGN a new item into the array with the INDEX function
+names[1] = "Mike"
+print names
+
+
+
+# SHOVEL: ADD A NEW ELEMENT TO AN ARRAY
+#print names
+#print "\n" # code for new line
+#names << "Brian" #add element to end of array -- shovel operation
+#names << "Abby"
+#print names
