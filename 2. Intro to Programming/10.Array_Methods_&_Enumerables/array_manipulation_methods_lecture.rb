947b41978f634a3d8f3536391cbea93e706e4140
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2020-01-25 22:35:13 -0500

    Add files via upload

---
+++ 2. Intro to Programming/10.Array_Methods_&_Enumerables/array_manipulation_methods_lecture.rb
@@ -0,0 +1,15 @@
+
+# << adds an aelement to the end of an array, evaluates to the array
+
+# array.push adds an element(s) to the end of an array, evaluates to teh array
+# array.pop removes the last element of an array, evaluates to the element that was removed
+
+# array.unshift adds an element to the front of an array, evaluates to the array
+# array.shift removes the first element of an array, evaluates to the element that was removed
+
+
+arr = ["Vanessa", "Candace", "Dave"]
+print arr.shift
+puts
+print arr # you can call it what it is
+puts
