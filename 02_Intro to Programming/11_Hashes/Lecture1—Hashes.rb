4e152dffd2179ec8dc55ff75fc899fba8fb444bb
Brian H. Hough <56947075+BrianHHough@users.noreply.github.com>
2020-03-10 19:28:01 -0400

    Update Lecture1—Hashes.rb

--- 02_Intro to Programming/11_Hashes/Lecture1—Hashes.rb
+++ 02_Intro to Programming/11_Hashes/Lecture1—Hashes.rb
@@ -1,1 +1,11 @@
-Test
+#Data Structure - Hashes
+# 'key => value' pairs
+
+my_hash = {"name" => "App Academy",
+    "color" => "red", 
+    "age" => 5, 
+    "isAwesome" => true,
+    42 => "hello"
+}
+puts my_hash[42]
+
